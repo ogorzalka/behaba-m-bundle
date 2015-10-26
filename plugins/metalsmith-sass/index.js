49a14e8397af025b5d282b5ba505dea7611b1d14
Olivier Gorzalka <olivier@behaba.com>
2015-10-26 11:02:51 +0100

    Update index.js

--- plugins/metalsmith-sass/index.js
+++ plugins/metalsmith-sass/index.js
@@ -127,5 +127,5 @@
   // exposing node-sass types for custom functions. see:
   // https://github.com/stevenschobert/metalsmith-sass/pull#21
   plugin.types = sass.types;
-  module.exports = plugin['default'];
+  module.exports = plugin;
 }());
